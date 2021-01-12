a5845ed36f4d00df47379aa1985959cf8aa50fbd
farooqiusman <ufben10@gmail.com>
2021-01-12 00:28:38 -0500

    Jan-12-2020 Latest rice stuff

---
+++ Scripts/Start_up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Terminate
+
+sudo tlp start
---
+++ Scripts/rofi.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+bg_color=#292C2F
+fg_color=#B18D68
+border_color=#36301f
+text_color=#f3f4f5
+htext_color=#B18D68
+
+rofi -show run -lines 3 -eh 2 -width 25 -padding 55 -opacity 30 -bw 5 -color-window "$bg_color, $fg_color, $border_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "Sans 18"
